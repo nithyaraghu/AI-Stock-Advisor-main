e1e8a2e52085dc029fe70d98c1df66697a392ca7
Nithyashree Raghunathan <143565205+nithya1316@users.noreply.github.com>
2026-05-14 15:02:43 -0700

    cleanup - remove old cosmos/chroma files

--- utils.py
+++
@@ -1,9 +0,0 @@
-# utils.py
-
-from sentence_transformers import SentenceTransformer
-
-# Initialize Embeddings model
-embedding_model = SentenceTransformer('all-MiniLM-L6-v2')
-
-def generate_embedding(text):
-    return embedding_model.encode([text])[0].tolist()
